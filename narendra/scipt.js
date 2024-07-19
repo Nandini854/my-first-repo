a1e377b4a96f165137fa58278ca90ffd5fbcc772
Nandini854 <nandinishri124@gmail.com>
2024-07-19 09:52:09 +0530

    Add files via upload

---
+++ narendra/scipt.js
@@ -0,0 +1,42 @@
+let n= 5;
+let m =4;
+let str = "5";
+
+console.log(n == m) // false
+console.log(n === str)//false 
+console.log(n > m)//true 
+console.log(n < m)//false
+console.log(n>=m) // n>m || n==m 
+console.log(n<=m) // n>m || n==m 
+console.log(n!=0)//true
+console.log(n!==m)
+
+
+
+let age=18;
+let canVote= age >= 18 ? 'Yes':'No';
+console.log(canVote); //Yes
+
+let isMember=true;
+let fee= isMember ? 2.00: 10.00;
+console.log(fee);
+
+
+console.log(5>4&&5<6)
+console.log(5>4||5<6)
+console.log(4>3!=5<6)
+
+
+ console.log(typeof"john")
+ console.log(typeof 3.14)
+console.log(typeof NnN)
+console.log(typeof false)
+console.log[1,2,3,4]
+console.log(typeof{ name:'john', age :34})
+console.log(new  Date())
+console.log(function() {})
+console.log(ByCar)
+console.log(null)
+
+
+
